493b0aeefbd697a6ad37ac5d1f27c862869103e8
manoj-kumbhar <kumbharmanoj244@gmail.com>
2025-11-18 10:31:25 +0530

    feat: update navbar styling and app adjustments before sync

--- New-Car-Loan/src/app/app.ts
+++ New-Car-Loan/src/app/app.ts
@@ -1,15 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/components/navbar/navbar';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet,Navbar],
 import { VehicleForm } from './features/vehicle-details/components/vehicle-form/vehicle-form';
 
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet,Navbar],
+// import { VehicleForm } from './features/vehicle-details/components/vehicle-form/vehicle-form';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,VehicleForm],
+  imports: [RouterOutlet, VehicleForm, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
